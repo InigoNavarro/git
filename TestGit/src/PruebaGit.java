3ada82710a08ecb2f4029544dc90407a4eb917f8
InigoNavarro <33113628+InigoNavarro@users.noreply.github.com>
2018-09-11 09:20:09 +0200

    borrar

--- TestGit/src/PruebaGit.java
+++ TestGit/src/PruebaGit.java
@@ -4,10 +4,6 @@ public class PruebaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Quiero subir.");
-		System.out.println("H.");
-		System.out.println("l");
-		System.out.println("e");
-		System.out.println("o");
 	}
 
 }
